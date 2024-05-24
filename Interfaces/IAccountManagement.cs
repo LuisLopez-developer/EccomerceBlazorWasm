687afe695744f2ab7deb5039d3cd388654fb6790
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-05-23 23:15:16 -0500

    se añadio un registro simple - temporal

---
+++ Interfaces/IAccountManagement.cs
@@ -0,0 +1,10 @@
+﻿using EccomerceBlazorWasm.Models;
+
+namespace EccomerceBlazorWasm.Interfaces
+{
+    public interface IAccountManagement
+    {
+        public Task<FormResult> RegisterAsync(string email, string password);
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,28 @@
+using EccomerceBlazorWasm;
+using EccomerceBlazorWasm.Interfaces;
+using EccomerceBlazorWasm.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using EccomerceBlazorWasm;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddScoped(sp => (IAccountManagement)sp.GetRequiredService<AuthenticationStateProvider>());
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(builder.Configuration["FrontendUrl"] ?? "https://localhost:7033")
+});
+
+
+builder.Services.AddHttpClient("Auth", opt => opt.BaseAddress =
+new Uri(builder.Configuration["BackendUrl"] ?? "http://localhost:55977/"));
+//   .AddHttpMessageHandler<CutomHttpHandler>();
 
 await builder.Build().RunAsync();

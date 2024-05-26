12f1b34fd65febda5923b781de39ea48f860781f
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-05-25 19:07:43 -0500

    se añadio los servicios para product brand

---
+++ Interfaces/IProductBrand.cs
@@ -0,0 +1,14 @@
+﻿using EccomerceBlazorWasm.Models.ViewModel;
+
+namespace EccomerceBlazorWasm.Interfaces
+{
+    public interface IProductBrand
+    {
+        Task<List<ProductBrandViewModel>> GetAllAsync();
+        Task<List<ProductBrandViewModel>> SearchAsync(string name);
+        Task<ProductBrandViewModel> GetByIdAsync(int id);
+        Task<ProductBrandViewModel> CreateAsync(ProductBrandViewModel productBrand);
+        Task<bool> UpdateAsync(int id, ProductBrandViewModel productBrand);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ Services/ProductBrandService.cs
@@ -0,0 +1,64 @@
+﻿using EccomerceBlazorWasm.Models.ViewModel;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+
+namespace EccomerceBlazorWasm.Services
+{
+    public class ProductBrandService
+    {
+        private readonly string api = "api/ProductBrand";
+
+        private readonly HttpClient _httpClient;
+
+        public ProductBrandService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("Auth");
+        }
+
+        private readonly JsonSerializerOptions jsonSerializerOptions =
+          new()
+          {
+              PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+          };
+
+        public async Task<ProductBrandViewModel> CreateAsync(ProductBrandViewModel productBrand)
+        {
+            var response = await _httpClient.PostAsJsonAsync<ProductBrandViewModel>($"{api}", productBrand);
+            var productbrandObject = await response.Content.ReadAsStringAsync();
+            var createProductBrand = JsonSerializer.Deserialize<ProductBrandViewModel>(productbrandObject, jsonSerializerOptions);
+            return createProductBrand;
+        }
+
+        public async Task<List<ProductBrandViewModel>> SearchAsync(string name)
+        {
+            var productBrands = await _httpClient.GetFromJsonAsync<List<ProductBrandViewModel>>($"{api}/search?name={name}");
+            return productBrands;
+        }
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var result = await _httpClient.DeleteAsync($"{api}/{id}");
+            return result.IsSuccessStatusCode;
+        }
+
+        public async Task<List<ProductBrandViewModel>> GetAllAsync()
+        {
+            var productBrand = await _httpClient.GetFromJsonAsync<List<ProductBrandViewModel>>($"{api}/GetAll");
+            return productBrand;
+        }
+
+        public async Task<ProductBrandViewModel> GetByIdAsync(int id)
+        {
+            var productCategory = await _httpClient.GetFromJsonAsync<ProductBrandViewModel>($"{api}/{id}");
+            return productCategory;
+        }
+
+        public async Task<bool> UpdateAsync(int id, ProductBrandViewModel productBrand)
+        {
+            var emptyContent = new StringContent(JsonSerializer.Serialize(productBrand), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PutAsync($"{api}/{id}", emptyContent);
+            return response.IsSuccessStatusCode;
+        }
+
+    }
+}

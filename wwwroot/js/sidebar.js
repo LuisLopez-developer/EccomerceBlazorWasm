3f2c920e5327203c965b2f74a01ef9677c5b759d
Fabrizzio Zambrano <fabrizziozv.17@gmail.com>
2024-05-25 23:59:53 -0500

    Se mejoro estructura de sidebar con submenus (falta separarlo por componente)

---
+++ wwwroot/js/sidebar.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const collapsibleButtons = document.querySelectorAll('[data-collapse-toggle]');
+
+    collapsibleButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const target = document.getElementById(this.getAttribute('aria-controls'));
+            target.classList.toggle('hidden');
+        });
+    });
+});

3effb1523ca43629866ea4149d37a21939aecd81
JHANPIERRE <jmunozmanayay@gmail.com>
2024-05-27 02:59:12 -0500

    Filtrado y listado de Entrada.

---
+++ Interfaces/IEntry.cs
@@ -0,0 +1,11 @@
+﻿using EccomerceBlazorWasm.Models.ViewModel;
+
+namespace EccomerceBlazorWasm.Interfaces
+{
+    public interface IEntry
+    {
+        Task<List<EntryViewModel>> GetAllAsync();
+
+        Task<List<EntryViewModel>> FilterByDateAsync(DateTime startDate, DateTime endDate);
+    }
+}
---
+++ Services/EntryService.cs
@@ -0,0 +1,43 @@
+﻿using EccomerceBlazorWasm.Interfaces;
+using EccomerceBlazorWasm.Models.ViewModel;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace EccomerceBlazorWasm.Services
+{
+    public class EntryService : IEntry
+    {
+
+        private readonly string api = "api/Entry";
+
+        private readonly HttpClient _httpClient;
+
+        public EntryService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("Auth");
+        }
+
+        private readonly JsonSerializerOptions jsonSerializerOptions = new()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        };
+
+
+        public async Task<List<EntryViewModel>> FilterByDateAsync(DateTime startDate, DateTime endDate)
+        {
+            var startDateFormatted = startDate.ToString("yyyy-MM-dd");
+            var endDateFormatted = endDate.ToString("yyyy-MM-dd");
+            var filterUrl = $"{api}/filter?startDate={startDateFormatted}&endDate={endDateFormatted}";
+
+            var entry = await _httpClient.GetFromJsonAsync<List<EntryViewModel>>(filterUrl);
+
+            return entry;
+        }
+
+        public async Task<List<EntryViewModel>> GetAllAsync()
+        {
+            var entry = await _httpClient.GetFromJsonAsync<List<EntryViewModel>>($"{api}/GetAll");
+            return entry;
+        }
+    }
+}

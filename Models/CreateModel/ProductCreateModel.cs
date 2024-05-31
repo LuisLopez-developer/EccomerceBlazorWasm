5c2520c19b165bc90911ada60f8cfe4336efd738
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-05-31 12:55:03 -0500

    mejora: se mejoro el formulario de crear producto

--- Models/CreateModel/ProductCreateModel.cs
+++ Models/CreateModel/ProductCreateModel.cs
@@ -5,15 +5,18 @@ namespace EccomerceBlazorWasm.Models.CreateModel
     public class ProductCreateModel
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Code { get; set; }
+        public string Name { get; set; }
+        public  string Code { get; set; }
         public int? StateId { get; set; }
         public DateTime? Date { get; set; }
-        public decimal? Price { get; set; }
+        public decimal Price { get; set; }
         public decimal? Cost { get; set; }
-        public int? Existence { get; set; }
-        public int? ProductBrandId { get; set; }
-        public int? ProductCategoryId { get; set; }
+        public int Existence { get; set; }
+        public bool IsVisible { get; set; } = false;
+        public string? Description { get; set; }
+        public string BarCode { get; set; } = "s/n";
+        public int ProductBrandId { get; set; }
+        public int ProductCategoryId { get; set; }
 
         public List<ProductPhotoViewModel>? Photos { get; set; }
         public ProductSpecificationViewModel? Specifications { get; set; }

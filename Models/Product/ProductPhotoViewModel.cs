cfbfa70858f5ec1bfc45fc8a3aa2e7e401db309d
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-06-02 20:08:05 -0500

    añadido la funcionalidad de poder escoger el archivo main

--- Models/Product/ProductPhotoViewModel.cs
+++ Models/Product/ProductPhotoViewModel.cs
@@ -3,8 +3,9 @@
     public class ProductPhotoViewModel
     {
         public int Id { get; set; }
+        public required string FileName { get; set; } = "SinNombre";
         public required string Url { get; set; }
-        public bool IsMain { get; set; }
-        public required int ProductId { get; set; }
+        public bool IsMain { get; set; } = false;
+        public int ProductId { get; set; }
     }
 }

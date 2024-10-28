cfdd37384cf0e2ce02ddb3791d8c30455a4ab54f
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-10-28 00:29:25 -0500

    Inicialización de propiedades y corrección de OrderDetails
    Se han inicializado las propiedades `CustomerDNI`, `CustomerEmail`, `WorkerId`, `Status` y `PaymentMethod` con valores predeterminados de cadena vacía (`string.Empty`) para evitar valores nulos.
    
    Además, se ha corregido la inicialización de la propiedad `OrderDetails` para que sea una lista vacía (`new List<OrderDetailViewModel>()` en lugar de `[]`), previniendo posibles errores de referencia nula.

--- Models/OrderViewModel.cs
+++ Models/OrderViewModel.cs
@@ -5,15 +5,15 @@ namespace EccomerceBlazorWasm.Models
     public class OrderViewModel
     {
         public int Id { get; set; }
-        public string CustomerDNI { get; set; }
-        public string CustomerEmail { get; set; }
-        public string WorkerId { get; set; }
+        public string CustomerDNI { get; set; } = string.Empty;
+        public string CustomerEmail { get; set; } = string.Empty;
+        public string WorkerId { get; set; } = string.Empty;
         public int StatusId { get; set; }
-        public string Status { get; set; } // descripción del estado
+        public string Status { get; set; } = string.Empty;
         public int PaymentMethodId { get; set; }
-        public string PaymentMethod { get; set; } // descripción del método de pago
+        public string PaymentMethod { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
         public decimal Total { get; set; }
-        public List<OrderDetailViewModel> OrderDetails { get; set; }
+        public List<OrderDetailViewModel> OrderDetails { get; set; } = [];
     }
 }

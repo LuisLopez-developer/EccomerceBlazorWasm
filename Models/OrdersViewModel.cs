ee726e1afeee703403e7f5a565bfaf90bec9a814
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-10-28 00:32:03 -0500

    Asignar valores predeterminados a propiedades en OrdersViewModel
    Se han realizado cambios en la clase `OrdersViewModel` dentro del archivo `OrdersViewModel.cs` del proyecto `EccomerceBlazorWasm.Models`. Las propiedades `UserName`, `CreatedByUserName`, `Status` y `PaymentMethod` ahora se inicializan con el valor predeterminado `string.Empty` en lugar de ser `string` sin valores predeterminados.

--- Models/OrdersViewModel.cs
+++ Models/OrdersViewModel.cs
@@ -3,10 +3,10 @@
     public class OrdersViewModel
     {
         public int Id { get; set; }
-        public string UserName { get; set; }
-        public string CreatedByUserName { get; set; }
-        public string Status { get; set; }
-        public string PaymentMethod { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string CreatedByUserName { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public string PaymentMethod { get; set; } = string.Empty;
         public decimal Total { get; set; }
         public DateTime CreatedAt { get; set; }
         public bool IsCreatedBySameUser { get; set; }
